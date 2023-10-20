03023562bbef9ef85f80dfda171a1b596fbcae78
Varun Jha <varun.jha@spense.money>
2023-10-20 14:20:50 +0530

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Varun on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
